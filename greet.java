dffd6ad97f1317a16c6ce827d1cbf3721d50a5b6
Prashanth <prashanthmandala48@gmail.com>
2023-04-25 11:11:57 +0530

    added 101 file changes

--- greet.java
+++ greet.java
@@ -1,8 +1,20 @@
 class greet{
 
+<<<<<<< Updated upstream
 int a=100;
 int b=200;
 
 syso("applied 202 changes");
+=======
+int i=10;
+int j=20;
+
+//task 101 changes
+
+m1()
+m2()
+
+
+>>>>>>> Stashed changes
 
 }
